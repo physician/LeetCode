0e543902a20f39c64248adecee3aa1e356d8849d
physician <feijj0121@gmail.com>
2013-08-29 11:47:14 -0500

    First implementation, use two pointers

---
+++ RemoveDuplicatesfromSortedArrayII.cpp
@@ -0,0 +1,62 @@
+/*
+ Author:     physician
+ Date:       Aug 29, 2013
+ Update:     Aug 29, 2013
+ Problem:    Remove Duplicates from Sorted Array II
+ Difficulty: Easy
+ Source:     http://leetcode.com/onlinejudge#question_80
+
+ Problem Description:
+ Remove Duplicates from Sorted Array II: 
+
+ Follow up for "Remove Duplicates":
+ What if duplicates are allowed at most twice?
+
+ For example,
+ Given sorted array A = [1,1,1,2,2,3],
+
+ Your function should return length = 5, and A is now [1,1,2,2,3].
+
+ Notes:
+ 1. First implementation, use two pointers.
+ 2. Single pass, time complexity O(N). In place, space complexity O(1).
+ 3. Be careful to handle special cases of input. Simplify logic.
+ 4. Compile using g++: g++ RemoveDuplicatesfromSortedArrayII.cpp -o RemoveDuplicatesfromSortedArrayII
+*/
+
+# include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
+        if (n == 0 || n == 1)
+            return n;
+        int i = 1;
+        int j = 2;
+        while (j < n) {
+            if (A[j] != A[i-1]) {
+                A[++i] = A[j];
+            }
+            j++;
+        }
+        return i + 1;
+    }
+};
+
+
+int main() {
+    //int A[6] = {1, 1, 1, 2, 2, 3};
+    int A[20] = {1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 4, 5, 6, 6, 6, 6, 6, 7, 8, 8};
+    cout << "Before duplication removal:" << endl;
+    for (int i = 0; i < sizeof(A)/sizeof(int); i++)
+        cout << A[i] << " ";
+    cout << endl;
+    Solution sol;
+    int len = sol.removeDuplicates(A, sizeof(A)/sizeof(int));
+    cout << "After duplication removal:" << endl;
+    for (int i = 0; i < len; i++)
+        cout << A[i] << " ";
+    cout << endl;
+}
