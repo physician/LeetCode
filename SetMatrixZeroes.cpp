bbf69858fa40d389f7f6ea1da4d8e14c06c37ea9
physician <feijj0121@gmail.com>
2014-02-05 00:16:41 -0600

    First implementation, refactor

--- SetMatrixZeroes.cpp
+++ SetMatrixZeroes.cpp
@@ -52,6 +52,7 @@ public:
                 }
             }
         }
+/*
         for (int i = 1; i < m; ++i) {
             if (matrix[i][0] == 0) {
                 for (int j = 1; j < n; ++j)
@@ -61,6 +62,13 @@ public:
         for (int j = 1; j < n; ++j) {
             if (matrix[0][j] == 0) {
                 for (int i = 1; i < m; ++i)
+                    matrix[i][j] = 0;
+            }
+        }
+*/
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
                     matrix[i][j] = 0;
             }
         }
